bd33225581c8ee2164d19c7c2de141d36ffe850b
bob.ying <124599792@qq.com>
2017-12-28 18:57:58 +0800

    Initialize version

---
+++ src/main/java/com/eyun/cloud/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.eyun.cloud.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.eyun.cloud")
+public class FeignConfiguration {
+
+}
